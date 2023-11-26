099b15604e512105168734d044ac621f93c229c9
Алексей Еременко <138244975+AlexDanish@users.noreply.github.com>
2023-11-26 23:43:44 +0600

    Add files via upload

---
+++ Целое число в интервале.py
@@ -0,0 +1,9 @@
+print('Целое число в интервале')
+print('Введите целое число')
+A = int(input())
+if 14<A<17:
+    print('True')
+elif -15<A<=12 or A>=19:
+    print('True')
+else:
+    print('False ')
